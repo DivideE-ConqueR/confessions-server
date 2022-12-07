105a23f37d0095f0db2377a56db9e2d7024731d3
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-12-07 22:42:18 +0530

    save post and hashtag simultaneously in a transaction

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { Router } from "express";
 import {
   uniqueNamesGenerator,
@@ -5,32 +6,70 @@ import {
   names,
 } from "unique-names-generator";
 import { Post } from "../models/Post.js";
+import Hashtag from "../models/Hashtag.js";
 
 const router = Router();
 
 router.post("/", async (req, res) => {
-  const newPost = new Post({
-    name: uniqueNamesGenerator({
-      dictionaries: [adjectives, names],
-      separator: " ",
-      style: "capital",
-    }),
-    ...req.body,
-  });
+  const session = await mongoose.startSession();
+  const transactionOptions = {
+    readPreference: "primary",
+    readConcern: { level: "local" },
+    writeConcern: { w: "majority" },
+  };
 
-  await newPost.save((err) => {
-    if (err) {
-      res.status(500).json({
-        status: "error",
-        message: `Error saving post - ${err.message}`,
+  try {
+    const transactionResults = await session.withTransaction(async () => {
+      const newPost = new Post({
+        name: uniqueNamesGenerator({
+          dictionaries: [adjectives, names],
+          separator: " ",
+          style: "capital",
+        }),
+        ...req.body,
+      });
+
+      const postsCreateResults = await newPost.save({ session });
+
+      await Hashtag.bulkWrite(
+        req.body.tags.map((tag) => {
+          return {
+            updateOne: {
+              filter: { hashtag: tag },
+              update: {
+                $setOnInsert: { hashtag: tag },
+                $push: { posts: postsCreateResults._id },
+              },
+              upsert: true,
+            },
+          };
+        }),
+        { session }
+      );
+    }, transactionOptions);
+
+    if (transactionResults) {
+      res.status(201).json({
+        status: "success",
+        message: "Post created successfully",
         data: null,
       });
     } else {
-      res
-        .status(201)
-        .json({ status: "success", message: "Post created", data: null });
+      res.status(500).json({
+        status: "error",
+        message: "Error saving post - Transaction failed",
+        data: null,
+      });
     }
-  });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: `Something went wrong - ${err.message}`,
+      data: null,
+    });
+  } finally {
+    await session.endSession();
+  }
 });
 
 router.get("/", (_req, res) => {
