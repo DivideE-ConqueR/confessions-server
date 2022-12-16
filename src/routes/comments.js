48da8533907d672ab849b00b1eb1e6c1edb379b2
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-12-16 20:43:38 +0530

    build: save comment and hashtags simultaneously in transaction

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -1,34 +1,82 @@
 import { Router } from "express";
+import mongoose from "mongoose";
 import {
   adjectives,
   names,
   uniqueNamesGenerator,
 } from "unique-names-generator";
+import extract from "mention-hashtag";
 import Comment from "../models/Comment.js";
+import Hashtag from "../models/Hashtag.js";
 
 const router = Router();
 
 router.post("/", async (req, res) => {
-  const newComment = new Comment({
-    name: uniqueNamesGenerator({
-      dictionaries: [adjectives, names],
-      separator: " ",
-      style: "capital",
-    }),
-    ...req.body,
-  });
+  let comment;
 
-  await newComment.save((err) => {
-    if (err) {
-      res
-        .status(500)
-        .json({ status: "error", message: "Error saving comment", data: null });
+  const session = await mongoose.startSession();
+  const transactionOptions = {
+    readPreference: "primary",
+    readConcern: { level: "local" },
+    writeConcern: { w: "majority" },
+  };
+
+  try {
+    const transaction = await session.withTransaction(async () => {
+      const newComment = new Comment({
+        name: uniqueNamesGenerator({
+          dictionaries: [adjectives, names],
+          separator: " ",
+          style: "capital",
+        }),
+        tag: extract(req.body.body, { type: "all", unique: true }),
+        //! Don't do this, it's a privacy issue. Be more specific.
+        ...req.body,
+      });
+
+      const savedComment = await newComment.save({ session });
+      comment = savedComment;
+
+      await Hashtag.bulkWrite(
+        newComment.tag.hashtags.map((tag) => {
+          return {
+            updateOne: {
+              filter: { hashtag: tag },
+              update: {
+                $setOnInsert: { hashtag: tag },
+              },
+              upsert: true,
+            },
+          };
+        }),
+        { session }
+      );
+    }, transactionOptions);
+
+    if (transaction) {
+      const { meta, ...others } = comment._doc;
+
+      res.status(201).json({
+        status: "success",
+        message: "Comment created successfully",
+        data: others,
+      });
     } else {
-      res
-        .status(201)
-        .json({ status: "success", message: "Comment created", data: null });
+      res.status(500).json({
+        status: "error",
+        message: "Error saving comment - Transaction failed",
+        data: null,
+      });
     }
-  });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: `Something went wrong - ${err.message}`,
+      data: null,
+    });
+  } finally {
+    await session.endSession();
+  }
 });
 
 router.get("/:id", (req, res) => {
