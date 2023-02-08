e3567369bc316219d71c0824331d7ea4c04c89ae
Debadyuti <debadyuti.karmakar@gmail.com>
2023-02-08 22:20:26 +0530

    search

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,52 +1,60 @@
-[
-  {
-    $search: {
-      compound: {
-        should: [
-          {
-            text: {
-              query: "rp",
-              path: "body",
-              fuzzy: {
-                maxEdits: 1,
-              },
-            },
+import { Router } from "express";
+import Post from "../models/Post.js";
+
+const router = Router();
+
+router.post("/", async (req, res) => {
+  const { q } = req.query;
+
+  const pipeline = [
+    {
+      $search: {
+        text: {
+          query: q,
+          path: ["body", "name"],
+          fuzzy: {
+            maxEdits: 1,
           },
-          {
-            text: {
-              query: "natural",
-              path: "name",
-              fuzzy: {
-                maxEdits: 1,
-              },
-            },
-          },
-        ],
+        },
       },
     },
-  },
-  {
-    $match: {
-      "meta.isDeleted": false,
-    },
-  },
-  {
-    $project: {
-      name: 1,
-      body: 1,
-      count: 1,
-      createdAt: 1,
-      updatedAt: 1,
-    },
-  },
-  {
-    $addFields: {
-      score: {
-        $meta: "searchScore",
+    {
+      $match: {
+        "meta.isDeleted": false,
       },
     },
-  },
-  {
-    $limit: 30,
-  },
-];
+    {
+      $project: {
+        name: 1,
+        body: 1,
+        count: 1,
+        engagement: 1,
+        score: {
+          $meta: "searchScore",
+        },
+        createdAt: 1,
+        updatedAt: 1,
+      },
+    },
+    {
+      $limit: 30,
+    },
+  ];
+
+  try {
+    const result = await Post.aggregate(pipeline);
+    res.status(200).json({
+      status: "success",
+      message: "Search results",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: `Error searching - ${error.message}`,
+      data: null,
+    });
+  }
+});
+
+export default router;
