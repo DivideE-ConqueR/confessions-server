f19dad34ed5ca7da5aa0b72bc4bd7c162b8bd64a
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2023-04-27 12:54:48 +0530

    remove cache middleware

--- src/middlewares/cache.js
+++
@@ -1,9 +0,0 @@
-const cache = (req, res, next) => {
-  if (req.method === "GET") {
-    res.setHeader("Cache-Control", "max-age=60, stale-while-revalidate=120");
-    // res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate=120");
-  }
-  next();
-};
-
-export { cache };
